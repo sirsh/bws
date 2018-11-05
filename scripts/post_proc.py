c34dadd44b2f3b4a0d5e0bbc99da7b433b938ce3
sirsh <s.amarteifio14@imperial.ac.uk>
2018-11-05 16:11:49 +0100

    init commit basic BWS code

--- scripts/post_proc.py
+++ scripts/post_proc.py
@@ -6,14 +6,14 @@ q = input("please provide a UNIQUE run qualifier for output files [e.g RUN07_90_
 
 print("processing the out.data file in the parent folder")
 template = "#(HIST)#"
-with open ("../out.data") as f:
-    with open("../data/{}_histogram.csv".format(q), "w") as g:
+with open ("./out.data") as f:
+    with open("./data/{}_histogram.csv".format(q), "w") as g:
         for l in f:
             if l[:len(template)] == template:
                 g.write(l[len(template):])  
 
 
-data = pd.read_csv("../out.data",sep='\t', comment='#',)
+data = pd.read_csv("./out.data",sep='\t', comment='#',)
 data["chunk"] = 1.0
 data = data[[ "chunk", "L","t", "M0","M1","M2","M3","M4","M5","M6","M7","M8"]]
 for m in ["M"+str(i+1) for i in range(8)]:   data[m] = data[m].astype(float) / data["M0"].astype(float)
@@ -26,6 +26,6 @@ std = grp.std().drop("chunk",1).drop("M0",1).drop("L",1) / np.sqrt(grp.count())#
 alldata = mn.join(std, rsuffix="error")
 cols = np.sort(alldata.columns.values)
 alldata = alldata[cols]
-alldata.to_csv("../data/{}_moments.csv".format(q))
+alldata.to_csv("./data/{}_moments.csv".format(q))
 
 print("files saved to /data/{}_".format(q))
